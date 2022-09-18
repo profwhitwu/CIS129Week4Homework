4b53e2f939b71ccd8eefa9dd4224cb95bd9b6686
Whit Wu <whit.wu@gmail.com>
2022-09-18 09:37:47 -0400

    Add assignment

---
+++ Week_4_Homework_Arrays_Lists_Dictionaries/Program.cs
@@ -0,0 +1,28 @@
+﻿// REMINDER, array indexs START AT 0 in C#.  So the first element will be of index 0.
+
+// 1. Create a single-dimensional array of type int.  Populate it with 6 numbers.
+
+// 2. Now pull out the 5th number in the array and store it in a variable of type int
+
+// 3. Create a multi-dimensional int array with two rows, each row being 5 elements long
+// Prepopulate it with data
+
+// 4.  Now pull out the 3rd number in the 2nd row of that array and store it in a variable
+
+// 5. Create a jagged array with 4 rows.  
+// Make the 1st row 3 columns long
+// Make the 2nd row 5 columns long
+// Make the 3rd row 2 columns long
+// Make the 4th row 4 columns long
+// Prepopulate each row with data
+
+// 6. Change the 3rd element in the 4th row.
+
+// 7. Create a list of type string
+
+// 8. Manually add 4 strings to the list.  Then remove the LAST 2 elements BY INDEX.
+
+// 9. Create a dictionary where the key is of type int and the value is of type string.
+// Populate it with 5 elements.
+
+// 10.  Print any two values from the dictionary to the console.  You should pull them by key.
